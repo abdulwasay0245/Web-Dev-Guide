52e3f5a5c177c58b813b023ec573310f0bbb2b4f
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-12 23:47:38 +0500

    build an age calculator

--- JavaScript/module_2/project.js
+++ JavaScript/module_2/project.js
@@ -1,7 +1,7 @@
 // In simple terms , variable are a container that holds a specific value;
 var age = 19;
 // this is called variable decleration
-console.log(age)
+// console.log(age)
 
 
 
@@ -19,13 +19,21 @@ var isLoggedIn = true;
 // Input birth year
 
 // Calculate and show age
+const date = new Date()
+const currentYear = date.getFullYear()
+
+
+// console.log(date.getFullYear())
 
 
 
+console.log("Enter your Birth Year");
 
-
+// process.stdout.write("Enter your Birth Year")
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Hello, ${name}!`);
+    const BirthYear = data;
+    const yourAge = currentYear - BirthYear
+    console.log(yourAge)
+ 
     process.exit(); // Exit the process after receiving input
   });
