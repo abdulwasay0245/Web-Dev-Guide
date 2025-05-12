e3d6e307c370a9d7042e2eb7f913213fe4664aa4
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-12 10:29:38 +0500

    added input

--- JavaScript/module_2.js/project.js
+++
@@ -1,3 +0,0 @@
-// In simple terms , variable are a container that holds a specific value;
-var age = 19;
-console.log(age)
---
+++ JavaScript/module_2/project.js
@@ -0,0 +1,17 @@
+// In simple terms , variable are a container that holds a specific value;
+var age = 19;
+// this is called variable decleration
+console.log(age)
+
+
+
+
+
+
+
+
+// process.stdin.on('data', (data) => {
+//     const name = data.toString().trim();
+//     console.log(`Hello, ${name}!`);
+//     process.exit(); // Exit the process after receiving input
+//   });
