43a913b96ff50d26cf304c80e93a62e987515662
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-17 06:33:42 +0500

    s

--- JavaScript/module_4/project.js
+++ JavaScript/module_4/project.js
@@ -5,10 +5,23 @@
 
 
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for (let index = 0; index <array.length; index++) {
-    const element = array[index];
-    console.log(element);
+// for (let index = 0; index <array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
     
+// }
+
+
+for (let index = 0; index < 140; index++) {
+    
+    setTimeout(() => {
+        console.log(` ${index}`);
+        
+    },  index * 300);
+    
+}
+while (true) {
+    console.log("hellow")
 }
